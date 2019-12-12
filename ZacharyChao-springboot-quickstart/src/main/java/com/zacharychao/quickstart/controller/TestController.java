71a109918df5ca692cd92f9ea834e20b51d7b7fe
ZacharyChao <41775247+ZacharyChao@users.noreply.github.com>
2019-12-12 23:45:32 +0800

    Initial commit

---
+++ ZacharyChao-springboot-quickstart/src/main/java/com/zacharychao/quickstart/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.zacharychao.quickstart.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class TestController {
+	@RequestMapping("/test")
+	@ResponseBody
+	public List<String> test() {
+		return Arrays.asList("1","2","3");
+	}
+}
