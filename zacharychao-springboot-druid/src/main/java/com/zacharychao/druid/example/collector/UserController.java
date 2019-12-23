4a5af61f8d2d5f789053600e3988b0a9848d6d11
ZacharyChao <41775247+ZacharyChao@users.noreply.github.com>
2019-12-24 00:14:19 +0800

    123

--- zacharychao-springboot-druid/src/main/java/com/zacharychao/druid/example/collector/UserController.java
+++ zacharychao-springboot-druid/src/main/java/com/zacharychao/druid/example/collector/UserController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zacharychao.druid.example.service.UserService;
 
@@ -14,6 +15,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	@RequestMapping("/getAllUser")
+	@ResponseBody
 	public List<Map<String,Object>> getAllUsers(){
 		return userService.getAllUsers();
 	}
